ccba22fb207d606e5521a62140ad6838722cc078
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-08-27 17:10:47 -0300

    feat(client): add basic implementation of dialog and websocket

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from 'react'
+import { socket } from './api/socket'
 import { MessageInput } from './components/MessageInput'
 import { Messages } from './components/Messages'
 import { Profiles } from './components/Profiles'
+
+import { LoginModal } from './components/LoginModal'
 import './styles/App.css'
 
 // import cursorImage from './assets/cursor.png'
 
 export function App() {
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected !')
+    })
+  }, [])
+  
+  
   return (
     <>
-
+      <LoginModal isOpen={true} />
       {/* <div className='cursor-container'>
         <img className="cursor-image" src={cursorImage} alt="" />
         <div className='cursor-name-tag'>Alan Jhonatan</div>
