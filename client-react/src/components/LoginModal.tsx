3ea233e714caede6fca5788951c28f16d405bbf3
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-08-28 19:10:24 -0300

    feat(client): add context implementation

--- client-react/src/components/LoginModal.tsx
+++ client-react/src/components/LoginModal.tsx
@@ -1,25 +1,53 @@
 import * as Dialog from '@radix-ui/react-dialog'
 
+import { ChangeEvent, useContext, useState } from 'react'
+import { ChatContext } from '../context/ChatContext'
 import '../styles/LoginModal.css'
 
-interface LoginModalProps {
-    isOpen: boolean
-}
+export function LoginModal() {
+    const { doLogin, isLoginEnabled } = useContext(ChatContext)
+    
+    const [name, setName] = useState<string>('')
+    const [isDisabled, setIsDisabled] = useState<boolean>(true)
 
-export function LoginModal(props: LoginModalProps) {
+    function handleNameInput(event: ChangeEvent<HTMLInputElement>) {
+        const currentName = event.target.value.trim()
+        setName(currentName)
+
+        if(currentName.length) {
+            setIsDisabled(false)
+            return
+        }
+
+        setIsDisabled(true)
+    }
+
+    function handleLogin() {
+        doLogin(name)
+    }
+    
     return (
-        <Dialog.Root open={props.isOpen}>
+        <Dialog.Root open={isLoginEnabled}>
             <Dialog.Trigger asChild>
                 <button>Logar</button>
             </Dialog.Trigger>
             <Dialog.Portal>
             <Dialog.Overlay className='modal-overlay' />
             <Dialog.Content className='modal-content'>
-                <Dialog.Title>
-                    Some random title
-                </Dialog.Title>
-                <Dialog.Description>Some random descriptions</Dialog.Description>
-                <Dialog.Close>X</Dialog.Close>
+                <div className='modal-header'>
+                    <Dialog.Title>Chat Login</Dialog.Title>
+                </div>
+
+                <div className='modal-body'>
+                    <label>Name:</label>
+                    <input
+                        type="text"
+                        onChange={handleNameInput}
+                        name='nickname'
+                        placeholder='insert your name here' 
+                    />
+                    <button disabled={isDisabled} onClick={handleLogin}>Enter</button>
+                </div>
             </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
