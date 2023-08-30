c523367f7984ab797d6b72cac714308cba131e89
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-08-30 17:52:49 -0300

    feat(server): add basic event handlers

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -1,4 +1,5 @@
 import { Server } from "socket.io"
+import { IProfile } from "./Types"
 
 const io = new Server(3000, {
     cors: {
@@ -6,8 +7,41 @@ const io = new Server(3000, {
     }
 })
 
+type SocketProfile = IProfile & { socketId: string }
+
+let socketProfiles: SocketProfile[] = []
+
+function listProfiles(profiles: SocketProfile[]) {
+    return profiles.map(profile => ({
+        id: profile.id,
+        user: profile.user
+    }))
+}
+
 io.on("connection", (socket) => {
-    console.log('new connection', socket)
+    socket.emit('initialProfiles', listProfiles(socketProfiles))
+    
+    socket.on("login", (data: IProfile) => {
+        const currentSocket = {
+            socketId: socket.id,
+            ...data,
+        }
+
+        socketProfiles.push(currentSocket)
+        socket.emit('addProfile', data)
+
+        console.log('login', socketProfiles)
+    })
+
+    socket.on("disconnect", () => {
+        socketProfiles = socketProfiles.filter(socketProfile => socket.id !== socketProfile.socketId)
+
+        socket.emit("updateProfiles", listProfiles(socketProfiles))
+        console.log('onDisconnect', socketProfiles)
+    })
 })
+
+
+
 
 io.listen(4000)
